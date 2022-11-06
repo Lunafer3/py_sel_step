3ddd8480b05da9b1e994229f494576686f9b8d32
Mikhail Bubnov <19lunafer97@gmail.com>
2022-11-06 09:35:51 +0300

    metod find.elements

--- test.py
+++ test.py
@@ -1,48 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+browser = webdriver.Firefox()
+browser.get("http://suninjuly.github.io/huge_form.html")
+elements = browser.find_elements_by_tag_name("input")
+for element in elements:
+    element.send_keys("Мой ответ")
 
-    def test_add_group(self):
-        wd = self.wd
-        wd.get("http://suninjuly.github.io/find_link_text")
-        wd.find_element_by_link_text("224592").click()      
-        wd.find_element_by_name("first_name").click()
-        wd.find_element_by_name("first_name").clear()
-        wd.find_element_by_name("first_name").send_keys("Иван")
-        wd.find_element_by_name("last_name").click()
-        wd.find_element_by_name("last_name").clear()
-        wd.find_element_by_name("last_name").send_keys("Иванов")
-        wd.find_element_by_xpath("/html/body/div[1]/form/div[3]/input").click()
-        wd.find_element_by_xpath("/html/body/div[1]/form/div[3]/input").clear()
-        wd.find_element_by_xpath("/html/body/div[1]/form/div[3]/input").send_keys("см")
-        wd.find_element_by_id("country").click()
-        wd.find_element_by_id("country").clear()
-        wd.find_element_by_id("country").send_keys("ru")
-        wd.find_element_by_xpath("//form/button").click()
-        time.sleep(30)
-        
+button = browser.find_element_by_css_selector("button.btn")
+button.click()
 
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
+alert = browser.switch_to.alert
+print(alert.text.split()[-1])
+alert.accept()
 
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+browser.close()
+browser.quit()
